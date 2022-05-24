b029df9c8584530422daddf7d2a0597c6e8851d2
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-05-24 16:33:42 +0200

    email post

---
+++ EmailApplication/Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace EmailApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body) 
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(body));
+            email.To.Add(MailboxAddress.Parse(body));
+            email.Subject = "Test email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = body }; 
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com");
+        }
+    }
+}
